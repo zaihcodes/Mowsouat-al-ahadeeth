ecc500af5101f6ea9fcfecdb37c12b4dd01a7eac
ZI-DreamWork <zaihwork@gmail.com>
2023-12-19 16:19:00 +0100

    Complete the category remote data source

--- lib/src/category/data/data source/category_remote_date_source.dart
+++ lib/src/category/data/data source/category_remote_date_source.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:hadeeth/core/error/exceptions.dart';
 import 'package:hadeeth/src/category/data/models/category_detail_model.dart';
 import 'package:hadeeth/src/category/data/models/category_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class CategoryRemoteDataSource {
   Future<List<CategoryModel>> getAllCategories({required String lang});
@@ -10,13 +14,24 @@ abstract class CategoryRemoteDataSource {
       required String perPage});
 }
 
-const BASE_URL = 'https://hadeethenc.com/api/v1/';
+const baseUrl = 'https://hadeethenc.com';
 
 class CategoryRemoteDataSourceImpl implements CategoryRemoteDataSource {
+  final http.Client client;
+  const CategoryRemoteDataSourceImpl({required this.client});
+
   @override
-  Future<List<CategoryModel>> getAllCategories({required String lang}) {
-    // TODO: implement getAllCategories
-    throw UnimplementedError();
+  Future<List<CategoryModel>> getAllCategories({required String lang}) async {
+    final url = Uri.parse('$baseUrl/api/v1/categories/list/?language=$lang');
+    final response = await client.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body)['data'];
+      return data.map((json) => CategoryModel.fromJson(json)).toList();
+    } else {
+      throw ServerException(
+          'Failed to load categories: ${response.reasonPhrase}');
+    }
   }
 
   @override
@@ -24,8 +39,17 @@ class CategoryRemoteDataSourceImpl implements CategoryRemoteDataSource {
       {required String lang,
       required String categoryId,
       required String page,
-      required String perPage}) {
-    // TODO: implement getCategoryDetail
-    throw UnimplementedError();
+      required String perPage}) async {
+    final url = Uri.parse(
+        '$baseUrl/api/v1/hadeeths/list/?language=$lang&category_id=$categoryId&page=$page&per_page=$perPage');
+    final response = await client.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body)['data'];
+      return data.map((json) => CategoryDetailModel.fromJson(json)).toList();
+    } else {
+      throw ServerException(
+          'Failed to load category Hadeeths: ${response.reasonPhrase}');
+    }
   }
 }
